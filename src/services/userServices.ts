ec27771a4651f02ebcd599827536f1015202b141
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2026-06-29 00:26:00 +0630

    add: userService to get user data directly from firestore

---
+++ src/services/userServices.ts
@@ -0,0 +1,36 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebaseConfig";
+
+export interface UserNrc{
+    nrc_front: string;
+    nrc_back: string;
+}
+
+export interface UserProfile {
+  uid: string;
+  email: string | null;
+  displayName: string;
+  phoneNumber: string | null;
+  role: string;
+  createdAt: any;
+  pushToken?: string;
+  nrc?: UserNrc;
+}
+
+export const userService = {
+  getUserProfile: async (uid: string): Promise<UserProfile | null> => {
+    try {
+      const userRef = doc(db, "users", uid);
+      const docSnap = await getDoc(userRef);
+
+      if (docSnap.exists()) {
+        return { uid: docSnap.id, ...docSnap.data() } as UserProfile;
+      }
+
+      return null;
+    } catch (error) {
+      console.error(`Error fetching profile for user ${uid}:`, error);
+      throw error;
+    }
+  }
+};
